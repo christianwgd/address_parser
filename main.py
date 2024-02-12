3156264657cb4de81f4ebbbbac2314b2dabe9585
Christian Wiegand <christianwgd@users.noreply.github.com>
2024-02-12 21:51:25 +0100

    Add api key security

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import json
 
 from deepparse.pre_processing import address_cleaner
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Security
 from deepparse.parser import AddressParser
-
+from fastapi.security import APIKeyHeader
+from starlette import status
 
 app = FastAPI()
 
 
-@app.get("/parse-address/{address}")
-async def say_hello(address: str):
+api_keys = [
+    "my_api_key"
+]
+
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header in api_keys:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+
+
+@app.get("/parse-address/")
+async def say_hello(address: str, api_key: str = Security(get_api_key)):
     address_parser = AddressParser(model_type="lightest", device="cpu")
     address = address_cleaner.coma_cleaning(address)
     address = address_cleaner.lower_cleaning(address)
