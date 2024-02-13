8b2c8a6211959e7f6474443e2234e7d26a808953
Christian Wiegand <christianwgd@users.noreply.github.com>
2024-02-13 11:55:38 +0100

    Proper error handling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Address Parser
 
 Addres parser based on [Deepparse](https://deepparse.org) and [FastAPI](https://fastapi.tiangolo.com).
+
+Run command: uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import json
 
 from deepparse.pre_processing import address_cleaner
-from fastapi import FastAPI, HTTPException, Security
+from fastapi import FastAPI, HTTPException, Security, Request, status
 from deepparse.parser import AddressParser
+from fastapi.exceptions import RequestValidationError
 from fastapi.security import APIKeyHeader
-from starlette import status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+
+
+routes_with_custom_exception = ['/']
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    if request.url.path in routes_with_custom_exception:
+        # check whether the error relates to the `some_custom_header` parameter
+        for err in exc.errors():
+            if err['loc'][0] == 'header' and err['loc'][1] == 'some-custom-header':
+                return JSONResponse(content={'401': 'Unauthorized'}, status_code=401)
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({'detail': exc.errors(), 'body': exc.body}),
+    )
 
 
 api_keys = [
