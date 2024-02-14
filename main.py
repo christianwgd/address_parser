989e0731a3e350a1d83ac78e272a3ee4df0fa91c
Christian Wiegand <christianwgd@users.noreply.github.com>
2024-02-14 19:12:15 +0100

    Receive address in request body instead of url

--- main.py
+++ main.py
@@ -46,7 +46,9 @@ def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
 
 
 @app.get("/parse-address/")
-async def say_hello(address: str, api_key: str = Security(get_api_key)):
+async def say_hello(request: Request, api_key: str = Security(get_api_key)):
+    request_json = await request.json()
+    address = request_json['address']
     address_parser = AddressParser(model_type="lightest", device="cpu")
     address = address_cleaner.coma_cleaning(address)
     address = address_cleaner.lower_cleaning(address)
@@ -55,11 +57,11 @@ async def say_hello(address: str, api_key: str = Security(get_api_key)):
     address = address_cleaner.hyphen_cleaning(address)
     adr_dict = address_parser(address).to_dict()
 
-    title_parts = ['StreetName', 'Municipality', 'Province']
+    title_parts = ['StreetName', 'Municipality']
     for part in title_parts:
-        adr_dict[part] = adr_dict[part].title() if adr_dict[part] is not None else "None"
+        adr_dict[part] = adr_dict[part].title() if adr_dict[part] is not None else None
     upper_case_parts = ['Province']
     for part in upper_case_parts:
-        adr_dict[part] = adr_dict[part].upper() if adr_dict[part] is not None else "None"
+        adr_dict[part] = adr_dict[part].upper() if adr_dict[part] is not None else None
     json_adr = json.dumps(adr_dict)
     return {"address": json_adr}
